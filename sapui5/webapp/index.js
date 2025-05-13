452563ac2c78e595fe4d60ac09664370df6d6425
LiudithReyes20 <liudithreyes@gmail.com>
2025-05-13 20:51:34 +0000

    05 Estrctra Proyectos SAPUI5

---
+++ sapui5/webapp/index.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/ComponentContainer"
+
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.ComponentContainer} ComponentContainer 
+     */
+    function (ComponentContainer) {
+
+        new ComponentContainer({
+            name: "alfagroup.sapui5",
+            settings: {
+                id: "sapui5"
+            },
+            async: true
+
+
+        }).placeAt("content");
+
+
+
+    });
